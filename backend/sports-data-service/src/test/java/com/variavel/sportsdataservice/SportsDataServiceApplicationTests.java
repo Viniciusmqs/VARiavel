793d5be766216d9d37ed04cb7e94142a58b33c12
vinicius marques <viniciusmarques291@gmail.com>
2025-05-29 14:59:35 -0300

    feat: Added user, gamification, and gateway microservices

---
+++ backend/sports-data-service/src/test/java/com/variavel/sportsdataservice/SportsDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.variavel.sportsdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
